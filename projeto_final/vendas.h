a3d099117238886a2adfcf627da29806b021b96a
Lorena Villagelin <villagelinlorena@gmail.com>
2025-11-30 18:16:32 -0300

    Refactor header and imprime functions for clarity

--- projeto_final/vendas.h
+++ projeto_final/vendas.h
@@ -4,9 +4,28 @@
 #include <string.h>
 #include <ctype.h>
 
+void limpa()
+{
+    fflush(stdin);
+    getchar();
+    system("cls");
+}
+
 void tab()
 {
     printf("\t\t\t");
+}
+
+void line()
+{
+    printf("\n\t\t--------------------------------------------------------------------------------\n");
+}
+
+void titulo(const char *nome)
+{
+    line(); tab(); tab();
+    printf("%s\n", nome);
+    line();
 }
 
 typedef struct data
@@ -105,21 +124,21 @@ void insere(Arv *a1, Dados d)
     }
 }
 
-//imprime os titulos da tabela
 void header()
 {
-    printf("| ID  | ");
-    printf("%-50s | ", "VENDEDOR");
-    printf("| COD |");
-    printf("%-50s | ", "CLIENTE");
-    printf("DATA       | ");
-    printf("VALOR      |\n");
+    printf("\n\n");
+    printf("| %-4s | ", "ID");
+    printf("%-30s | ", "VENDEDOR");
+    printf("%-4s | ", "COD");
+    printf("%-30s | ", "CLIENTE");
+    printf("%-10s | ", "DATA");
+    printf("%-12s |\n", "VALOR");
 }
 
-//imprime os cadastros
 void imprime(Dados d)
 {
-    printf("%4d | ", d.id);
+    header();
+    printf("| %4d | ", d.id);
     printf("%-30s | ", d.vendedor);
     printf("%-4s | ", d.numvendedor);
     printf("%-30s | ", d.cliente);
@@ -129,77 +148,63 @@ void imprime(Dados d)
 
 NoArv* remover_aux(NoArv *pai, int num)
 {
-    if(pai==NULL)
+    if (pai == NULL)
     {
-        printf("\n \n não encontrado na árvore");
+        return NULL;
+    }
+
+    if (num > pai->info.id)
+    {
+        pai->dir = remover_aux(pai->dir, num);
+    }
+    else if (num < pai->info.id)
+    {
+        pai->esq = remover_aux(pai->esq, num);
     }
     else
     {
-        if(num > pai->info.id)
+        if (pai->esq == NULL && pai->dir == NULL)
         {
-            pai->dir= remover_aux(pai->dir,num);
+            free(pai);
+            return NULL;
         }
-        else
+        if (pai->esq == NULL)
         {
-            if(num < pai->info.id)
-            {
-                pai->esq = remover_aux(pai->esq,num);
-            }
-            /*else achou o nó a ser removido*/
-            {
-                if(pai->dir==NULL && pai->esq==NULL) /* No sem filhos */
-                {
-                    free(pai); pai=NULL;
-                }
-                else
-                {
-                    if (pai->esq==NULL) /*so tem filho da direita */
-                    {
-                        NoArv *aux=pai;
-                        pai = pai->dir;
-                        free(aux);
-                    }
-                    else
-                    {
-                        if(pai->dir==NULL)/* so tem filho da esquerda */
-                        {
-                            NoArv *aux=pai;
-                            pai = pai->esq;
-                            free(aux);
-                        }
-                        else /* tem os dois filhos */
-                        {
-                            NoArv *aux;
-                            aux = pai->esq;
-                            while (aux->dir !=NULL)
-                            {
-                                aux=aux->dir;
-                            }
-                            pai->info.id = aux->info.id; /* troca as informações */
-                            aux->info.id = num;
-                            pai->esq = remover_aux(pai->esq,num);
-                        }
-                    }
-                }
-            }// achou o nó a ser removido
+            NoArv *aux = pai->dir;
+            free(pai);
+            return aux;
         }
-    } //else do não estar vazio
+        if (pai->dir == NULL)
+        {
+            NoArv *aux = pai->esq;
+            free(pai);
+            return aux;
+        }
+        NoArv *aux = pai->esq;
+        while (aux->dir != NULL)
+            aux = aux->dir;
+
+        pai->info = aux->info;
+        pai->esq = remover_aux(pai->esq, aux->info.id);
+    }
+
     return pai;
 }
 
-
-
 Arv* remover(Arv *RAIZ, int num)
 {
-    NoArv *aux=RAIZ->raiz;
-    if(aux->info.id==num && aux->dir==NULL && aux->esq==NULL)
-    {
-        free(aux);
-        free(RAIZ);
-        return NULL;
-    }
-    RAIZ->raiz = remover_aux(RAIZ->raiz,num);
+    RAIZ->raiz = remover_aux(RAIZ->raiz, num);
     return RAIZ;
+}
+
+void header_vendas()
+{
+    printf("\n\n");
+    tab();
+    printf("| %-4s | ", "ID");
+    printf("%-30s | ", "CLIENTE");
+    printf("%-10s | ", "DATA");
+    printf("%-12s |\n\n", "VALOR");
 }
 
 void BuscaVendas(NoArv *pai, char busca[], int *Vendas)
@@ -219,18 +224,34 @@ void BuscaVendas(NoArv *pai, char busca[], int *Vendas)
     if(strcmp(pai->info.vendedor,busca)==0)
     {
         (*Vendas) = 1;
-        printf("\n\n");
-        printf("%d ", pai->info.id);
-        printf("%s | ", pai->info.cliente);
-        printf("%d/%d/%d | ", pai->info.trans.dia, pai->info.trans.mes, pai->info.trans.ano);
-        printf("%.2f ", pai->info.valor);
+        tab();
+        printf("| %4d | ", pai->info.id);
+        printf("%-30s | ", pai->info.cliente);
+        printf("%02d/%02d/%04d | ", pai->info.trans.dia, pai->info.trans.mes, pai->info.trans.ano);
+        printf("R$%-10.2f |\n", pai->info.valor);
     }
 }
 
+int BuscaID(NoArv *pai, int id, int resul)
+{
+    if(pai->info.id == id)
+    {
+        resul = 1;
+    }
+    if (pai->dir != NULL)
+    {
+        resul = BuscaID(pai->dir, id, resul);
+    }
+    if (pai->esq != NULL)
+    {
+        resul = BuscaID(pai->esq, id, resul);
+    }
+    return resul;
+}
 
 void Decrescente(NoArv *pai)
 {
-      if (pai->dir != NULL)
+    if (pai->dir != NULL)
     {
         Decrescente(pai->dir);
     }
@@ -254,47 +275,51 @@ void Crescente(NoArv *pai)
     }
 }
 
-void ValoresAbaixo(NoArv *pai, float num)
+void ValoresAbaixo(NoArv *pai, float num, int *valores)
 {
      if (pai->dir != NULL)
     {
-        ValoresAbaixo(pai->dir, num);
+        ValoresAbaixo(pai->dir, num, valores);
     }
     if (pai->esq != NULL)
     {
-        ValoresAbaixo(pai->esq, num);
+        ValoresAbaixo(pai->esq, num, valores);
     }
     if(pai->info.valor < num)
     {
+        header();
+        (*valores) = 1;
         printf("\n\n");
-        printf("%d ", pai->info.id);
-        printf("%-20s | ", pai->info.vendedor);
-        printf("%-20s | ", pai->info.numvendedor);
-        printf("%-20s | ", pai->info.cliente);
-        printf("%d/%d/%d | ", pai->info.trans.dia, pai->info.trans.mes, pai->info.trans.ano);
-        printf("%.2f ", pai->info.valor);
+        printf("| %4d | ", pai->info.id);
+        printf("%-30s | ", pai->info.vendedor);
+        printf("%-4s | ", pai->info.numvendedor);
+        printf("%-30s | ", pai->info.cliente);
+        printf("%02d/%02d/%04d | ", pai->info.trans.dia, pai->info.trans.mes, pai->info.trans.ano);
+        printf("R$%-10.2f |\n", pai->info.valor);
     }
 }
 
-void ValoresAcima(NoArv *pai, float num)
+void ValoresAcima(NoArv *pai, float num, int *valores)
 {
      if (pai->dir != NULL)
     {
-        ValoresAcima(pai->dir, num);
+        ValoresAcima(pai->dir, num, valores);
     }
     if (pai->esq != NULL)
     {
-        ValoresAcima(pai->esq, num);
+        ValoresAcima(pai->esq, num, valores);
     }
     if(pai->info.valor > num)
     {
+        header();
+        (*valores) = 1;
         printf("\n\n");
-        printf("%d ", pai->info.id);
-        printf("%-20s | ", pai->info.vendedor);
-        printf("%-20s | ", pai->info.numvendedor);
-        printf("%-20s | ", pai->info.cliente);
-        printf("%d/%d/%d | ", pai->info.trans.dia, pai->info.trans.mes, pai->info.trans.ano);
-        printf("R$%.2f ", pai->info.valor);
+        printf("| %4d | ", pai->info.id);
+        printf("%-30s | ", pai->info.vendedor);
+        printf("%-4s | ", pai->info.numvendedor);
+        printf("%-30s | ", pai->info.cliente);
+        printf("%02d/%02d/%04d | ", pai->info.trans.dia, pai->info.trans.mes, pai->info.trans.ano);
+        printf("R$%-10.2f |\n", pai->info.valor);
     }
 }
 
@@ -340,7 +365,26 @@ int gera (void)
 }
 
 void geraCodigo(char *codigo, char letra) {
-    int numero = 100 + rand() % 900; // gera número de 3 dígitos (100–999)
-    sprintf(codigo, "%c%d", letra, numero); // junta a letra e o número
+    int numero = 100 + rand() % 900;
+    sprintf(codigo, "%c%d", letra, numero);
     codigo[4]='\0';
+}
+
+void libera_arvore(NoArv *pai)
+{
+    if (pai != NULL)
+    {
+        libera_arvore(pai->esq);
+        libera_arvore(pai->dir);
+        free(pai);
+    }
+}
+
+void libera_estrutura(Arv *arv)
+{
+    if (arv != NULL)
+    {
+        libera_arvore(arv->raiz);
+        free(arv);
+    }
 }
